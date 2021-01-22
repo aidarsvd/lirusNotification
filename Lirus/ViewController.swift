99f8c55800289b00d3e2c6600d0253951a710ae4
Aidar <achakiev2003@gmail.com>
2021-01-22 21:33:00 +0600

    Initial Commit

---
+++ Lirus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lirus
+//
+//  Created by Айдар Чакиев on 22/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
